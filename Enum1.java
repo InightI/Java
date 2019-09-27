edc4a6204b695bf82eb4ff056346c24d4d08b4de
Enight <3063976212@qq.com>
2019-09-27 20:00:19 +0800

    小练习1

---
+++ Enum1.java
@@ -0,0 +1,27 @@
+package lx201927;
+import java.util.*;
+
+enum Gender
+{
+    FEMALE,MALE,斯卡蒂
+}
+
+enum Week
+{
+    mon, th,fir
+}
+public class Enum1
+{
+    public static void main(String[] args)
+    {
+        Gender gender = Gender.斯卡蒂;
+        Gender o = Gender.斯卡蒂;
+        System.out.println(o);
+        Week xingqiyi = Week.mon;
+        System.out.println(xingqiyi);
+
+
+
+
+    }
+}
