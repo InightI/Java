c8f6b7ac8fa251a2fe3e6f680ea51d38975ccd67
Enight <3063976212@qq.com>
2019-11-11 22:17:23 +0800

    中断异常1

---
+++ 新建文件夹/InterruptThread.java
@@ -0,0 +1,30 @@
+package lesson2;
+
+public class InterruptThread {
+
+    public static void main(String[] args) {
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        thread.start();
+//        thread.interrupt();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+
+                }
+            }
+        });
+        thread2.start();
+        thread2.interrupt();
+    }
+}
