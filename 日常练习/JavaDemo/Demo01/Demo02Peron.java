f7576a57f526e23466b6b3702e53f7d7c4b43dd1
Enight <3063976212@qq.com>
2020-02-25 00:09:55 +0800

    Demo01/Demo02Peron.java

---
+++ 日常练习/JavaDemo/Demo01/Demo02Peron.java
@@ -0,0 +1,13 @@
+package Demo.Demo01;
+
+public class Demo02Peron
+{
+    public static void main(String[] args)
+    {
+        Person per1 = new Person();
+        per1.name = "sikadi";
+        per1.setAge(18);
+        per1.show();
+        System.out.println(per1.getAge());
+    }
+}
