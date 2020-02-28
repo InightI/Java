eff64c356f3a2cc94e10039ed1447f9796c14981
Enight <3063976212@qq.com>
2020-02-28 22:28:07 +0800

    Demo01Extends/Demo02warrior.java

---
+++ 日常练习/JavaDemo/Demo05Extends/Demo01Extends/Demo02warrior.java
@@ -0,0 +1,14 @@
+package Demo.Demo05Extends.Demo01Extends;
+
+public class Demo02warrior
+{
+
+    public void pikan()
+    {
+        System.out.println("劈砍");
+    }
+    public void shangtiao()
+    {
+        System.out.println("上挑");
+    }
+}
