8d215ef552d2705c4b4aa56e13d9546a16f8919c
Enight <3063976212@qq.com>
2020-02-28 22:31:51 +0800

    Demo01Extends/Father.java

---
+++ 日常练习/JavaDemo/Demo05Extends/Demo01Extends/Father.java
@@ -0,0 +1,13 @@
+package Demo.Demo05Extends.Demo01Extends;
+
+public class Father
+{
+    public Father()
+    {
+        System.out.println("我是父类无参构造");
+    }
+    public Father(int num)
+    {
+        System.out.println("我是父类有参构造");
+    }
+}
