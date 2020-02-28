e2d2c1f4cd16dd9711074f765e966b3b3903296f
Enight <3063976212@qq.com>
2020-02-28 22:29:55 +0800

    Demo01Extends/Demo03UpWarrior.java

---
+++ 日常练习/JavaDemo/Demo05Extends/Demo01Extends/Demo03UpWarrior.java
@@ -0,0 +1,12 @@
+package Demo.Demo05Extends.Demo01Extends;
+
+public class Demo03UpWarrior extends Demo02warrior
+{
+    public void warrior()
+    {
+        super.pikan();
+        super.shangtiao();
+
+        System.out.println("升级后会魔法啦");
+    }
+}
