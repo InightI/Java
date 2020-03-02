ee185e695c68827816f56cb9bf3f9aa4679980a3
Enight <3063976212@qq.com>
2020-03-02 16:23:28 +0800

    Demo05/Fashi.java

---
+++ 日常练习/JavaDemo/Demo05Extends/Demo05/Fashi.java
@@ -0,0 +1,11 @@
+package Demo.Demo05Extends.Demo05;
+
+public class Fashi
+{
+    String oneFashi = "法师类会法术";
+    int num = 20;
+    public void fashi()
+    {
+        System.out.println("我是法师类-父类");
+    }
+}
