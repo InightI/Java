1a06097645e00c46df7fd87548758aa9d0a97677
Enight <3063976212@qq.com>
2020-03-04 20:55:07 +0800

    demo01/demo02/MyInterfaceA.java

---
+++ 日常练习/JavaDemo/demo06Interface/demo01/demo02/MyInterfaceA.java
@@ -0,0 +1,11 @@
+package Demo.demo06Interface.demo01.demo02;
+
+public interface MyInterfaceA
+{
+    public void methodA();
+    public abstract void  methodAb();
+    public static void methodAstatic()
+    {
+        System.out.println("我是接口A的静态方法");
+    }
+}
