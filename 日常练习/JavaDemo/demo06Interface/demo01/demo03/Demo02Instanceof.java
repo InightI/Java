6ffd8f572112216a4704f1c19e7b5fd478479e81
Enight <3063976212@qq.com>
2020-03-05 21:38:25 +0800

    demo01/demo03/Demo02Instanceof.java

---
+++ 日常练习/JavaDemo/demo06Interface/demo01/demo03/Demo02Instanceof.java
@@ -0,0 +1,9 @@
+package Demo.demo06Interface.demo01.demo03;
+/*
+如何才能知道一个父类引用的对象,本来是什么子类?
+对象名 instanceof 类名称     animal instanceof Dog
+结果是一个boolean值,也就是判断前面的对象能不能当做后面类型的实例
+ */
+public class Demo02Instanceof
+{
+}
