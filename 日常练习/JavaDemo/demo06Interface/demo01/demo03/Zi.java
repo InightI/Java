859dc2d38a310b3ba8e80c0e38a81c632ea00010
Enight <3063976212@qq.com>
2020-03-05 21:39:12 +0800

    demo01/demo03/Zi.java

---
+++ 日常练习/JavaDemo/demo06Interface/demo01/demo03/Zi.java
@@ -0,0 +1,14 @@
+package Demo.demo06Interface.demo01.demo03;
+
+public class Zi extends Fu
+{
+    int num = 5;
+    public void method()
+    {
+        System.out.println(num);
+    }
+    public void methodZi()
+    {
+        System.out.println("子类特有方法");
+    }
+}
