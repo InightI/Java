68bafeab2ecb8009f3649802e2c74130f88012aa
Enight <3063976212@qq.com>
2020-03-06 20:54:36 +0800

    demo01/demo04Usb/KeyBoard.java

---
+++ 日常练习/JavaDemo/demo06Interface/demo01/demo04Usb/KeyBoard.java
@@ -0,0 +1,22 @@
+package Demo.demo06Interface.demo01.demo04Usb;
+//键盘
+public class KeyBoard implements USB
+{
+    @Override
+    public void open()
+    {
+        System.out.println("打开键盘");
+    }
+
+    @Override
+    public void close()
+    {
+        System.out.println("关闭键盘");
+    }
+
+    public void type()
+    {
+        System.out.println("键盘输入");
+    }
+
+}
