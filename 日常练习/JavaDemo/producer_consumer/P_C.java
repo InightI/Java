08069c5a82c57d8d20aba8e347765e1fc426ccf3
Enight <3063976212@qq.com>
2020-03-17 21:47:31 +0800

    producer_consumer/P_C.java

---
+++ 日常练习/JavaDemo/producer_consumer/P_C.java
@@ -0,0 +1,121 @@
+package javademo.producer_consumer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P_C
+{
+    private static final int CAPACITY = 6;
+
+    public static void main(String[] args)
+    {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Thread producer1 = new Producer("P1", queue, CAPACITY);
+        Thread producer2 = new Producer("P2", queue, CAPACITY);
+        Thread consumer1 = new Consumer("C1", queue, CAPACITY);
+        Thread consumer2 = new Consumer("C2", queue, CAPACITY);
+
+
+        producer1.start();
+        producer2.start();
+        consumer1.start();
+        consumer2.start();
+    }
+
+    public static class Producer extends Thread
+    {
+        private Queue<Integer> queue;
+        String name;
+        int maxSize;
+        int i = 0;
+
+        public Producer(String name, Queue<Integer> queue, int maxSize)
+        {
+
+            this.name = name;
+            this.queue = queue;
+            this.maxSize = maxSize;
+        }
+
+        public void run()
+        {
+            while(true)
+            {
+                synchronized (queue)
+                {
+                    while(queue.size() == maxSize)
+                    {
+                        System.out.println("队列已满,"+ name + "生产者停止生产");
+                        try
+                        {
+                            queue.wait();
+                        }
+                        catch (InterruptedException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("队列未满,生产者 " + name + "生产 " + i);
+                    queue.offer(i++);
+                    queue.notifyAll();
+                    try
+                    {
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Consumer extends Thread
+    {
+        private Queue<Integer> queue;
+        String name;
+        int maxSize;
+
+        public Consumer(String name, Queue<Integer> queue, int maxSize)
+        {
+            super(name);
+            this.name = name;
+            this.queue = queue;
+            this.maxSize = maxSize;
+        }
+
+        public void run()
+        {
+            while(true)
+            {
+                synchronized (queue)
+                {
+                    while(queue.isEmpty())
+                    {
+                        System.out.println("队列是空的");
+                        try
+                        {
+                            queue.wait();
+                        }
+                        catch (InterruptedException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    int x = queue.poll();
+                    System.out.println("队列有元素"+ name + "消费者开吃");
+                    queue.notifyAll();
+                    try
+                    {
+                        Thread.sleep(1000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
