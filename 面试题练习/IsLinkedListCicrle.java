4422815e33b364e04f0e7c663405ae0f0a9b05cf
Enight <3063976212@qq.com>
2020-03-24 00:29:31 +0800

    判断链表是否有环

---
+++ 面试题练习/IsLinkedListCicrle.java
@@ -0,0 +1,40 @@
+package com.HomeWork;
+
+
+public class IsLinkedListCicrle
+{
+    public boolean IsCycle(ListNode head)
+    {
+        if(head == null || head.next == null)
+        {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+
+
+class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
