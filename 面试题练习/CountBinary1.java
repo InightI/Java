3c8079af8a0d794f0c38d74db2d7c4ee0eb79007
Enight <3063976212@qq.com>
2020-04-25 00:14:08 +0800

    出现多少个1

---
+++ 面试题练习/CountBinary1.java
@@ -0,0 +1,19 @@
+package interviewWork;
+
+public class CountBinary1
+{
+    public int hammingWeight(int n)
+    {
+        String str = Integer.toBinaryString(n);
+        int count = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == '1')
+            {
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
