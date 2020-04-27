02d7d69772294ec8cdc01a71073af2ddb7699329
Enight <3063976212@qq.com>
2020-04-27 19:53:04 +0800

    判断二叉树

---
+++ 面试题练习/IsBinaryTree.java
@@ -0,0 +1,26 @@
+package interviewWork;
+//验证搜索二叉树 使用上下界
+public class IsBinaryTree
+{
+
+    class Solution
+    {
+        public boolean helper(TreeNode node, Integer lower, Integer upper)
+        {
+            if (node == null) return true;
+
+            int val = node.val;
+            if (lower != null && val <= lower) return false;
+            if (upper != null && val >= upper) return false;
+
+            if (!helper(node.right, val, upper)) return false;
+            if (!helper(node.left, lower, val)) return false;
+            return true;
+        }
+
+        public boolean isValidBST(TreeNode root)
+        {
+            return helper(root, null, null);
+        }
+    }
+}
