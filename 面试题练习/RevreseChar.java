c7b675c74b76e02bcdcb9874e8de3f4b222eeed2
Enight <3063976212@qq.com>
2020-05-16 21:24:19 +0800

    RevreseChar.java

---
+++ 面试题练习/RevreseChar.java
@@ -0,0 +1,18 @@
+package interviewWork;
+
+public class RevreseChar
+{
+    public void reverseChar(char arr[])
+    {
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right)
+        {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
